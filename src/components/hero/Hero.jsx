16f3ebbf40bd7986c61ccaf2b3366342db5e76aa
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-01-28 17:54:00 +0300

    Build hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,11 +1,57 @@
-import styles from './Hero.module.css'
+import styles from "./Hero.module.css";
 
-function Hero () {
-    return (
-        <section className={styles.hero}>
-            <h1>hello world</h1>
-        </section>
-    )
+function Hero() {
+  return (
+    <section className={styles.hero}>
+      <div className="container grid">
+        <div className={`${styles.img1} ${styles.imgContainer}`}>
+          <img
+            className={styles.img}
+            src="https://www.themealdb.com/images/media/meals/okl9cm1764371087.jpg"
+            alt="Torta de fiambre"
+          />
+        </div>
+        <div className={`${styles.img2} ${styles.imgContainer}`}>
+          <img
+            className={styles.img}
+            src="https://www.themealdb.com/images/media/meals/f3ee3y1763309332.jpg"
+            alt=""
+          />
+        </div>
+        <div className={`${styles.searchBar} searchBar`}>
+          <img
+            src="/search-icon.svg"
+            alt=""
+            style={{ width: "32px", height: "32px" }}
+          />
+          <input type="text" placeholder="Search meal by name" />
+        </div>
+        <h1 className={styles.welcomeText}>Welcome to the Meal<br /> Search Tool</h1>
+        <p className={styles.welcomeSubText}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi fuga fugiat harum. Cupiditate doloribus neque adipisci quas voluptatibus cumque explicabo.</p>
+        <div className={`${styles.img3} ${styles.imgContainer}`}>
+          <img
+            className={styles.img}
+            src="https://www.themealdb.com/images/media/meals/ewcikl1614348364.jpg"
+            alt=""
+          />
+        </div>
+        <div className={`${styles.img4} ${styles.imgContainer}`}>
+          <img
+            className={styles.img}
+            src="https://www.themealdb.com/images/media/meals/rqvwxt1511384809.jpg"
+            alt=""
+          />
+        </div>
+        <div className={`${styles.img5} ${styles.imgContainer}`}>
+          <img
+            className={styles.img}
+            src="https://www.themealdb.com/images/media/meals/tqtywx1468317395.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
